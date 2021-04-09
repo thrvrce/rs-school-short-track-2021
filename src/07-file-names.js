aebc5466ad63a2930a566d0b2f910ddc154d52d8
Victor Avdeev <avdeevvictor95@gmail.com>
2021-04-09 15:19:42 +0300

    feat: 07-file-names

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,34 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const newArr = [...names];
+
+  newArr.forEach((name, index) => {
+    if (newArr.indexOf(name) !== index) {
+      const regExp = new RegExp(`^${name}\\([1-9]*\\)$`);
+      const arrOfRenamedFiles = newArr
+        .filter((value, valueIndex) => regExp.test(value) && valueIndex < index);
+
+      if (arrOfRenamedFiles.length) {
+        const maxNumber = arrOfRenamedFiles.reduce(
+          (maxNumberAcc, value) => {
+            const currentNameAdditionalNumber = Number(value.match(/.*\((?<number>[1-9]*)\)$/).groups.number);
+
+            return maxNumberAcc < currentNameAdditionalNumber
+              ? currentNameAdditionalNumber
+              : maxNumberAcc;
+          }, 0,
+        );
+
+        newArr[index] = `${name}(${maxNumber + 1})`;
+      } else {
+        newArr[index] = `${name}(1)`;
+      }
+    }
+  });
+
+  return newArr;
 }
 
 module.exports = renameFiles;
